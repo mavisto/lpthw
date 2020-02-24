ad1b1dcd6e41055dab779b7354e72e918f043280
mavisto <dsmavisto@gmail.com>
2020-02-24 13:59:22 +0000

    Book Exercises

---
+++ ex5.py
@@ -0,0 +1,19 @@
+my_name = 'David J Smith'
+my_age = 35 # This is a lie
+my_height = 76 # Invhes
+my_weight = 250 # lbs
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown' # What's left of it
+
+print(f"Let's talk about {my_name}")
+print(f"He's {my_height} inches tall")
+print(f"He's {my_weight} pounds heavy")
+print("Need to loose some")
+print(f"He's got {my_eyes} eyes and {my_hair} hair")
+print(f"His teeth are usually {my_teeth} depending on the beer")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
+print("Rounding up or down?", round(1.4))
