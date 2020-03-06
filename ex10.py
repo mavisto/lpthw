81c18aaba78ca159d12884a78c5cb76a21379431
David Smith <DASM8@hscic.gov.uk>
2020-03-06 10:41:25 +0000

    more book stuff

---
+++ ex10.py
@@ -0,0 +1,27 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split \non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+smelly_cat = """
+\\23 \n
+\'23 \n
+\"23 \n
+\a23 \n
+\b23 \n
+\f23 \n
+\r23 \n
+\t23 \n
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(smelly_cat)
