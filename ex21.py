41bdc41bd55093f105f531f7b3bcb38e26b47efe
David Smith <DASM8@hscic.gov.uk>
2020-03-25 13:36:33 +0000

    Even more exercises

---
+++ ex21.py
@@ -0,0 +1,33 @@
+def add(a, b):
+    print(f"ADDING {a} + {b}")
+    return a + b
+
+def subtract(a, b):
+    print(f"SUBTRACING {a} - {b}")
+    return a - b
+
+def multiply(a, b):
+    print(f"MULTIPLYING {a} * {b}")
+    return a * b
+
+def divide(a, b):
+    print(f"DIVIDING {a} / {b}")
+    return a / b
+
+
+print("Let's do something with functions!")
+
+age = add(50,7)
+height = subtract(100, 22)
+weight =  multiply(76, 3)
+iq = divide(300,2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+
+#A puzzle for the extra credit, type it anyway
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(iq,2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
